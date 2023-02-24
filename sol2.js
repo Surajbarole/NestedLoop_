184672fc2110a927dcacb72a6076abe552a0cc80
Suraj Barole <103420352+Surajbarole@users.noreply.github.com>
2023-02-24 13:58:40 +0000

    Initial commit

---
+++ sol2.js
@@ -0,0 +1,16 @@
+// PROBLEM 2
+// Problem 2: Print Prime Numbers from 1 to given limit
+
+for(let i=1;i<=20;i++){
+  let count=0;
+  for(let j=1;j<=i;j++){
+    if(i%j==0){
+      count++;
+    }
+  }
+  if(count==2){
+    console.log(i,"Its prime")
+  }else {
+    console.log(i,"NOt prime")
+  }
+}
